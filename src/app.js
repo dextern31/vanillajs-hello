eb554ea222e62d9937ce3d737d1ec2eb2005f45e
dextern31 <140563233+dextern31@users.noreply.github.com>
2023-08-05 01:20:21 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    function random(array) {
+      return array[Math.floor(Math.random() * array.length)];
+    }
+    return (
+      random(who) +
+      " " +
+      random(action) +
+      " " +
+      random(what) +
+      " " +
+      random(when)
+    );
+  }
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
 };
